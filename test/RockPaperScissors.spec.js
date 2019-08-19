acd653135e0f84529c5a6f10e016e2bf8cebe575
Daenam Kim <daenam.kim@gmail.com>
2019-08-19 14:06:14 +0900

    Add tests for enroll

--- test/RockPaperScissors.spec.js
+++ test/RockPaperScissors.spec.js
@@ -1,9 +1,38 @@
-var RockPaperScissors = artifacts.require("RockPaperScissors");
+const RockPaperScissors = artifacts.require('RockPaperScissors');
+const truffleAssert = require('truffle-assertions');
+const { toBN, toWei } = web3.utils;
 
-contract("RockPaperScissors", function(accounts) {
-  it("should assert true", function(done) {
-    var rock_paper_scissors = RockPaperScissors.deployed();
-    assert.isTrue(true);
-    done();
+contract('RockPaperScissors', function(accounts) {
+  const [owner, alice, bob] = accounts;
+
+  let rpsInstance;
+  beforeEach(async () => {
+    rpsInstance = await RockPaperScissors.new({ from: owner });
+  });
+
+  it('should avoid to enroll with insufficient value', async () => {
+    await truffleAssert.fails(
+      rpsInstance.enroll({
+        from: alice,
+        value: '10'
+      }),
+      'Enroll must at least 100 wei'
+    );
+  });
+
+  it('should enroll successfully', async () => {
+    const value = toWei('1', 'ether');
+    const result = await rpsInstance.enroll({
+      from: alice,
+      value
+    });
+    assert.strictEqual(result.logs[0].event, 'LogEnrolled');
+    assert.strictEqual(result.logs[0].args.player, alice);
+    assert.strictEqual(result.logs[0].args.value.toString(), value);
+
+    const playerAlice = await rpsInstance.players(alice, {
+      from: alice
+    });
+    assert.strictEqual(playerAlice.toString(), toWei('1', 'ether'));
   });
 });
